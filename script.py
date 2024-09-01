b3b73fbb8e1080109480beca7bdbeb3c388e948a
Tinesife <tinesife94@gmail.com>
2024-09-01 14:34:43 -0300

    feat: engine analisys data

--- script.py
+++ script.py
@@ -1,6 +1,9 @@
 from pprint import pprint
+import asyncio
 
+import chess
 import chess.pgn
+import chess.engine
 import pandas as pd
 
 GAMER = 'ChaosJournaler'
@@ -54,9 +57,9 @@ while True:
     prev_board_fen = 'invalid fen'
 
     for move in game.mainline_moves():
-        board_fen = board.fen()
         move_san = board.san(move)
         board.push(move)
+        board_fen = board.fen()
 
         my_turn = (
                 (board.turn == chess.WHITE and playing_white) or 
@@ -73,6 +76,7 @@ while True:
                 oponent_move_dict[board_fen] = {}
                 my_move_dict[board_fen] = {}
             game_dict[board_fen][key] += 1
+            game_dict[board_fen]['initial_fen'] = prev_board_fen
 
             try:
                 oponent_move_dict[board_fen][move_san] += 1
@@ -105,7 +109,51 @@ for k, v in my_move_dict.items():
             prev_max_value = value
     game_dict[k]['my_most_popular_move'] = prev_max_key
 
-data = pd.DataFrame.from_dict(game_dict, orient='index')
-data['position_importance'] = (data.win_qtd + 2 * data.draw_qtd * 4 * data.loss_qtd) / 7
-# data = data.sort_values('loss_qtd', ascending=False)
-data.to_parquet(f'{FILE_NAME}.parquet')
+
+
+async def engine_evaluation(prepared_dict: dict) -> dict:
+    transport, engine = await chess.engine.popen_uci('/usr/bin/stockfish')
+    print('Configuring engine...')
+    await engine.configure({'Hash': 12288, 'Threads': 4})
+    start, finish = 0, len(prepared_dict)
+
+    result = {}
+    for k, v in prepared_dict.items():
+        start += 1
+        print(f'Analyzing position {start} of {finish} ({start * 100 / finish:.2f}%)...')
+        board = chess.Board(k)
+        print(f'\t- Position FEN: {board.fen()}')
+        print(f'\t- My move: {v["my_most_popular_move"]}')
+        original_info = await engine.analyse(board, chess.engine.Limit(depth=20))
+        try:
+            top_move = board.san(original_info['pv'][0])
+        except KeyError:
+            top_move = ''
+        turn = original_info['score'].turn
+        if top_move != v['my_most_popular_move'] and v['my_most_popular_move'] != '' and top_move != '':
+            board.push_san(v['my_most_popular_move'])
+            my_info = await engine.analyse(board, chess.engine.Limit(depth=20))
+            aux = {
+                'engine_top_move': top_move,
+                'after_my_move_evalutation_result': my_info["score"].wdl().pov(turn).expectation(),
+                'before_my_move_evaluation_result': original_info["score"].wdl().pov(turn).expectation(),
+            }
+        else:
+            aux = {
+                'engine_top_move': top_move,
+            }
+        print(f'\t- Engine top move: {top_move}')
+        result[k] = v | aux
+        if start % 200 == 0:
+            print('Salvando resultado parcial...')
+            part = pd.DataFrame.from_dict(result, orient='index')
+            part.to_parquet(f'{FILE_NAME}_engine_data_part.parquet')
+    return result
+
+game_df = pd.DataFrame.from_dict(game_dict, orient='index')
+game_df['position_importance'] = (game_df.win_qtd + 2 * game_df.draw_qtd * 4 * game_df.loss_qtd) / 7
+game_df = game_df.sort_values('loss_qtd', ascending=False)
+game_df.to_parquet(f'{FILE_NAME}_game_data.parquet')
+engine_dict = asyncio.run(engine_evaluation(game_df.to_dict(orient='index')))
+engine_df = pd.DataFrame.from_dict(engine_dict, orient='index')
+engine_df.to_parquet(f'{FILE_NAME}_engine_data.parquet')
